2ff2113d130bf60bcf19e9bed589e4a46cdea04c
Ivo Aguiar Pimenta <ivo.pimenta1@hp.com>
2025-10-10 18:20:56 -0300

    feature: add async method and validations

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@ import requests
 from os import getenv
 from dotenv import load_dotenv
 from fastapi import HTTPException
+import aiohttp
 
 load_dotenv(override=True)
 API_KEY = getenv('APHAVANTAGE_API_KEY')
@@ -15,7 +16,24 @@ def sync_converter(from_currency: str, to_currency: str, price:float):
     
     data = response.json()
     if "Realtime Currency Exchange Rate" not in data:
-        raise HTTPException(status_code=400, detail='Realtime currency exchange rate not found')
+        raise HTTPException(status_code=400, detail=f'Realtime currency exchange rate not found {data}')
+    
+    exchange_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+    
+    converted_price = price * exchange_rate
+    return converted_price
+
+async def async_converter(from_currency: str, to_currency: str, price:float):
+    url  = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=error)
+    
+    if "Realtime Currency Exchange Rate" not in data:
+        raise HTTPException(status_code=400, detail=f'Realtime currency exchange rate not found {data}')
     
     exchange_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
     
--- routers.py
+++ routers.py
@@ -1,7 +1,47 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Path
+from convert import sync_converter, async_converter
+from typing import Any, Annotated
+from asyncio import gather
+
 
 router = APIRouter()
 
-@router.get('/convert/ {from_currency}')
-def convert(from_currency: str, to_currencies: str, price: float):
-    return {"message": "Convert endpoint"}
+
+router = APIRouter(prefix='/converter')
+
+@router.get('/{from_currency}')
+def convert(
+        from_currency: str = Path(..., min_length=3, max_length=3, description="Currency code (e.g., USD, EUR)", pattern="^[A-Z]{3}$"), 
+        price: float = Query(..., gt=0, description="Amount to convert"), 
+        to_currencies: list[Annotated[str, Query(min_length=3, max_length=3, pattern="^[A-Z]{3}$")]] = Query(default=['USD'], min_length=1, max_length=5, description="List of target currency codes")
+    ) -> list[Any]:
+    result: list[Any] = []
+    for currency in to_currencies:
+        response = sync_converter(
+            from_currency = from_currency, 
+            to_currency= currency, 
+            price = price
+        )
+
+        result.append(response)
+
+    return result
+
+@router.get('/async/{from_currency}')
+async def async_convert(
+        from_currency: str = Path(..., min_length=3, max_length=3, description="Currency code (e.g., USD, EUR)", pattern="^[A-Z]{3}$"),
+        price: float = Query(..., gt=0, description="Amount to convert"), 
+        to_currencies: list[Annotated[str, Query(min_length=3, max_length=3, pattern="^[A-Z]{3}$")]] = Query(default=['USD'], min_length=1, max_length=5, description="List of target currency codes")
+    ) -> list[Any]:
+    coroutines: list[Any] = []
+    for currency in to_currencies:
+        coro =  async_converter(
+            from_currency = from_currency, 
+            to_currency= currency, 
+            price = price
+        )
+
+        coroutines.append(coro)
+
+    results = await gather(*coroutines)
+    return results
